5a07e0659d93eb7434a983aa6c33eadf6fe28c71
Semenetti <adriano.semenetti@gmail.com>
2020-02-19 22:39:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+exports.min = function min(array) {
+    return array && array.length > 0 ? array.sort((a, b) => b - a).pop() : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return array && array.length > 0 ? array.sort((a, b) => a - b).pop() : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return array && array.length > 0
+        ? array.reduce((a, b) => a + b, 0) / array.length
+        : 0;
+};
